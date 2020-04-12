dd8398b0b1b6ff92c334de213307ff9577fd7bf5
apt-x4869 <amanpasi.btech.cs17@iiitranchi.ac.in>
2020-04-12 12:58:12 +0530

    Import Book from resources.book

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from resources.user import UserRegister, UserLogin, UserLogout, TokenRefresh
+from resources.user import UserRegister, UserLogin, UserLogout, TokenRefresh, User
+from resources.book import Book
 from blacklist import BLACKLIST
 
 from db import db
@@ -19,6 +20,12 @@ def create_tables():
 
 jwt = JWTManager(app)
 
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1:
+        return {'is_admin': True}
+    return {'is_admin': False}
+
 @jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
     return decrypted_token['jti'] in BLACKLIST
@@ -27,6 +34,8 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(UserLogin, '/login')
 api.add_resource(UserLogout,'/logout')
 api.add_resource(TokenRefresh,'/refresh')
+api.add_resource(User,'/user/<int:user_id>')
+api.add_resource(Book, '/book/<string:title>')
 if __name__ == '__main__':
     db.init_app(app)
     app.run(port=5000, debug=True)
