65849b5473ef9c617cc7d26f362e21c832cc75b0
apt-x4869 <amanpasi.btech.cs17@iiitranchi.ac.in>
2020-04-12 11:44:38 +0530

    JWT Blacklist variable set to True

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from resources.user import UserRegister
+from resources.user import UserRegister, UserLogin, UserLogout, TokenRefresh
+from blacklist import BLACKLIST
 
 from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['JWT_SECRET_KEY'] = 'AMAN'
+app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
@@ -17,9 +19,14 @@ def create_tables():
 
 jwt = JWTManager(app)
 
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['jti'] in BLACKLIST
 
-api.add_resource(UserRegister,'/register')
-
+api.add_resource(UserRegister, '/register')
+api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout,'/logout')
+api.add_resource(TokenRefresh,'/refresh')
 if __name__ == '__main__':
     db.init_app(app)
     app.run(port=5000, debug=True)
