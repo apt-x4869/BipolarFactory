5712136054b4f70448b94647c42dcba902291ee6
apt-x4869 <amanpasi.btech.cs17@iiitranchi.ac.in>
2020-04-12 10:55:25 +0530

    Create DB befare first operation

--- app.py
+++ app.py
@@ -8,12 +8,17 @@ from db import db
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['JWT_SECRET_KEY'] = 'AMAN'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 jwt = JWTManager(app)
 
 
-api.resource(UserRegister,'/register')
+api.add_resource(UserRegister,'/register')
 
 if __name__ == '__main__':
     db.init_app(app)
