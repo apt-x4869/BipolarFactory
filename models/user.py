df4faf3d7c4b654f979ea8fad19813d18652cf8c
apt-x4869 <amanpasi.btech.cs17@iiitranchi.ac.in>
2020-04-12 09:50:33 +0530

    user.py created, added md5 password saving

---
+++ models/user.py
@@ -0,0 +1,35 @@
+from db import db
+import hashlib
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(1000))
+
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = hashlib.md5(password.encode()).hexdigest()
+
+    def json(self):
+        return {
+            'id': self.id,
+            'username': self.username
+        }
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
